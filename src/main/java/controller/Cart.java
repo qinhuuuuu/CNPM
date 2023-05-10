edeb1ae1322cbb70e278825ad58c6d7cdaa4b1fd
Phan Thị Quỳnh Như <20130356@st.hcmuaf.edu.vn>
2023-05-10 19:45:26 +0700

    Initial commit

---
+++ src/main/java/controller/Cart.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Cart", value = "/cart")
+public class Cart extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("cart.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
